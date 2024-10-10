e123a0a7646f09582e8c8d3d5e7b552789b660a4
collajuan <collajuan@gmail.com>
2024-10-10 14:11:06 +0200

    TaskCard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,24 @@
+import TaskCard from "@/components/TaskCard";
 import { prisma } from "@/libs/prisma";
 
-
 async function loadTasks() {
-//!Obtengo tareas haciendo uso de la API
+  //!Obtengo tareas haciendo uso de la API
   // const res = await fetch("http://localhost:3000/api/task");
   // const data = await res.json();
-//!Obtengo tareas haciendo consulta directo a la BBDD
-  const data = await prisma.task.findMany()
+  //!Obtengo tareas haciendo consulta directo a la BBDD
+  const data = await prisma.task.findMany();
   return data;
 }
 
 async function HomePage() {
   const tasks = await loadTasks();
   return (
-    <section className='container mx-auto'>
-      <div className='grid grid-cols-3 gap-3 mt-10' >
-      {tasks.map((task) => (
-        <div key={task.id} className='bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer relative' >
-          <h3 className='font-bold text-2xl mb-2'>{task.id}</h3>
-          <h3 className='font-bold text-2xl mb-2' >
-            {task.title}
-          </h3>
-          <p className='mb-5' >{task.description}</p>
-          <p className='text-gray-500 absolute bottom-0 right-0' >{new Date(task.cratedAt).toLocaleDateString()}</p>
-        </div>
-      ))}
-    </div>
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+         <TaskCard key={task.id} task={task}/> 
+        ))}
+      </div>
     </section>
   );
 }
