add2eb1127c43e6d5650b2e8fcd3d19da3c4e480
collajuan <collajuan@gmail.com>
2024-10-10 16:11:17 +0200

    button y metodo DELETE onClick

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -74,9 +74,31 @@ function NewPage({ params }) {
           onChange={(e) => setDescription(e.target.value)}
           value={description}
         ></textarea>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Crear
-        </button>
+        <div className="flex justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Crear
+          </button>
+          {params.id && (
+            <button
+              type="button"
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={async () => {
+                const res = await fetch(`/api/task/${params.id}`, {
+                  method: "DELETE",
+                });
+                const data = await res.json();
+                console.log(data);
+                router.refresh();
+                router.push("/");
+              }}
+            >
+              Delete
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
