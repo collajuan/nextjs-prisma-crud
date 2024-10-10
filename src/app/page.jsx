0eb285ab891e3d509b97622129171107aab5f28b
collajuan <collajuan@gmail.com>
2024-10-10 12:45:21 +0200

    loadTasks - uso de API vs leer BBDD con prisma

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
-  return (
-    <div>Tareas</div>
-  )
+
+async function loadTasks() {
+//!Obtengo tareas haciendo uso de la API
+  // const res = await fetch("http://localhost:3000/api/task");
+  // const data = await res.json();
+//!Obtengo tareas haciendo consulta directo a la BBDD
+  const data = await prisma.task.findMany()
+  return data;
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>
+            {task.id} - {task.title}
+          </h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default HomePage;
