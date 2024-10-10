618eb37fb8126aee10a22d5c10e5a9d8ab04f330
collajuan <collajuan@gmail.com>
2024-10-10 15:26:02 +0200

    useState, useEffect

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,31 +1,56 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
-function NewPage() {
-  const router = useRouter()
+function NewPage({ params }) {
+  const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  //si entro por parametro, cargo el formulario con los datos segun el id del parametro
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/task/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
+    //Si el formulario se cargo con paramtetro...voy a editar
+    if (params.id) {
+      const res = await fetch(`/api/task/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      //Si no fue con parametro voy a crear nueva tarea
+      const res = await fetch("/api/task", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "aplication/json",
+        },
+      });
+      const data = await res.json();
+    }
 
-    const res = await fetch("/api/task", {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "aplication/json",
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-    
-    router.push('/')
+    router.refresh();
+    router.push("/");
   };
 
   return (
     <div className="h-screen flex justify-center items-center">
-      <form className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2"
-       onSubmit={onSubmit}>
+      <form className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2" onSubmit={onSubmit}>
         <label htmlFor="title" className="font-bold text-sm">
           Título de la tarea
         </label>
@@ -34,6 +59,8 @@ function NewPage() {
           id="title"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           placeholder="Título"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label htmlFor="description" className="font-bold text-sm">
           Descripción de la tarea
@@ -44,6 +71,8 @@ function NewPage() {
           id="description"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           placeholder="Describe la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Crear
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,18 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 function TaskCard({task}) {
+    const router = useRouter()
+
   return (
     <div    
       className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer relative"
-    >
+      onClick={()=> router.push('/tasks/edit/' + task.id)}>
       <h3 className="font-bold text-2xl mb-2">{task.id}</h3>
       <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
       <p className="mb-5">{task.description}</p>
       <p className="text-gray-500 absolute bottom-2 right-2">
-        {new Date(task.cratedAt).toLocaleDateString()}
+      {new Date(task.cratedAt).toDateString()}
       </p>
     </div>
   );
